22042ec3f8cdd66d5cbabbe91967306b54cf264e
Jon Cabot <jfc0053@gmail.com>
2022-07-14 14:06:43 -0500

    completed assignment, need update

--- src/features/restaurants/RestaurantInput.js
+++ src/features/restaurants/RestaurantInput.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { restaurantAdded } from "./restaurantsSlice"
 
 function RestaurantInput() {
-  return <div>Restaurant Input</div>;
+  const [name, setName] = useState("");
+  const dispatch = useDispatch();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    dispatch(restaurantAdded(name));
+    setName("");
+  }
+
+  function handleChange(e) {
+    setName(e.target.value)
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name
+        <input 
+          type="text" 
+          onChange={handleChange} 
+          value={name}
+          name="name"
+        />
+      </label>
+      <button type="submit">Add Restaurant</button>
+    </form>
+  );
 }
 
 export default RestaurantInput;
